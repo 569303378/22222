1fe998f9e87b1b5ae07d453c949f6b8943ddae5a
Apple <569303378@qq.com>
2016-08-30 08:37:08 +0800

    增加一行注释+一个log

--- yyyyy/yyyyy/ViewController.m
+++ yyyyy/yyyyy/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"增加一个log");
 }
 
 - (void)didReceiveMemoryWarning {
